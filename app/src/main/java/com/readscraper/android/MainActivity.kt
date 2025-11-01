c31778b0fc1d612fab86404d45281e561e11856c
MMuArFF <muarf@protonmail.com>
2025-11-01 13:49:55 +0100

    Fix: ajouter accolade manquante dans MainActivity

--- app/src/main/java/com/readscraper/android/MainActivity.kt
+++ app/src/main/java/com/readscraper/android/MainActivity.kt
@@ -203,6 +203,7 @@ class MainActivity : ComponentActivity() {
                                 )
                             }
                         }
+                        }
                     }
                 }
             }
